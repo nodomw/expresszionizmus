b78c1a92c30b718bc1fa43ff0841bbac9ce07615
Notheisz Mátyás <nodomatyi@gmail.com>
2025-11-21 09:57:04 +0100

    rendeles done

--- routes/rendeles.ts
+++ routes/rendeles.ts
@@ -3,17 +3,95 @@ import pool from "../pool.ts";
 import * as z from "zod";
 
 const route = express.Router();
+const id = z.number().nonnegative();
 const model = z.object({
-  razon: z.number().nonnegative(),
   vazon: z.number().nonnegative(),
   fazon: z.number().nonnegative(),
   idopont: z.string().nonempty(),
 });
 
-route.get("/", (req, res) => {});
-route.get("/:id", (req, res) => {});
-route.post("/", (req, res) => {});
-route.put("/", (req, res) => {});
-route.delete("/", (req, res) => {});
+route.get("/", (req, res) => {
+  try {
+    pool
+      .execute("select * from rendeles")
+      .then((result) => res.status(200).json(result))
+      .catch((e) => {
+        res.status(500).send(e);
+      });
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+route.get("/:id", (req, res) => {
+  try {
+    pool
+      .execute("select * from rendeles where razon = ?", [
+        id.parse(Number(req.params.id)),
+      ])
+      .then((result) => res.status(200).json(result))
+      .catch((e) => {
+        res.status(404).send(e);
+      });
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+route.post("/", (req, res) => {
+  try {
+    const rendeles = model.parse(req.body);
+
+    pool
+      .execute(
+        "insert into rendeles (vazon, fazon, idopont) values (?, ?, ?)",
+        [rendeles.vazon, rendeles.fazon, rendeles.idopont],
+      )
+      .then((r) => res.status(201).json(r))
+      .catch((e) => {
+        res.status(500).send(e);
+      });
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+route.put("/:id", (req, res) => {
+  try {
+    const rendeles = model.parse(req.body);
+
+    pool
+      .execute(
+        "update rendeles set vazon = ?, fazon = ?, idopont = ? where razon = ?",
+        [
+          rendeles.vazon,
+          rendeles.fazon,
+          rendeles.idopont,
+          id.parse(Number(req.params.id)),
+        ],
+      )
+      .then((r) => res.status(200).json(r))
+      .catch((e) => {
+        res.status(500).send(e);
+      });
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+route.delete("/:id", (req, res) => {
+  try {
+    pool
+      .execute("delete from rendeles where razon = ?", [
+        id.parse(Number(req.params.id)),
+      ])
+      .then((r) => res.status(200).json(r))
+      .catch((e) => {
+        res.status(500).send(e);
+      });
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
 
 export default route;
