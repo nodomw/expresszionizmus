40f96ddb0445fdf916aa08dbcce84289f65f4520
Notheisz Mátyás <nodomatyi@gmail.com>
2025-11-21 10:07:58 +0100

    tetel done

--- routes/tetel.ts
+++ routes/tetel.ts
@@ -3,20 +3,95 @@ import pool from "../pool.ts";
 import * as z from "zod";
 
 const route = express.Router();
+const id = z.number().nonnegative();
 const model = z
   .object({
-    pazon: z.number().nonnegative(),
-    razon: z.number().nonnegative(),
+    pazon: z.number().nonnegative().optional(),
+    razon: z.number().nonnegative().optional(),
     db: z.number().nonnegative(),
   })
-  .refine((m) => m.db <= 1, {
+  .refine((m) => m.db >= 1, {
     message: "nem lehet 1-nél kevesebb pizzát kiadni",
   });
 
-route.get("/", (req, res) => {});
-route.get("/:id", (req, res) => {});
-route.post("/", (req, res) => {});
-route.put("/", (req, res) => {});
-route.delete("/", (req, res) => {});
+route.get("/", (req, res) => {
+  try {
+    pool
+      .execute("select * from tetel")
+      .then((result) => res.status(200).json(result))
+      .catch((e) => {
+        res.status(500).send(e);
+      });
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+route.get("/:id", (req, res) => {
+  try {
+    pool
+      .execute("select * from tetel where razon = ?", [Number(req.params.id)])
+      .then((result) => res.status(200).json(result))
+      .catch((e) => {
+        res.status(404).send(e);
+      });
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+route.post("/", (req, res) => {
+  try {
+    const tetel = model.parse(req.body);
+
+    pool
+      .execute("insert into tetel (pazon, razon, db) values (?, ?, ?)", [
+        tetel.pazon,
+        tetel.razon,
+        tetel.db,
+      ])
+      .then((r) => res.status(201).json(r))
+      .catch((e) => {
+        res.status(500).send(e);
+      });
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+route.put("/:razon/:pazon", (req, res) => {
+  try {
+    const tetel = model.parse({
+      pazon: Number(req.params.pazon),
+      razon: Number(req.params.razon),
+      ...req.body,
+    });
+
+    pool
+      .execute("update tetel set  db = ? where razon = ? and pazon = ?", [
+        tetel.db,
+        tetel.razon,
+        tetel.pazon,
+      ])
+      .then((r) => res.status(200).json(r))
+      .catch((e) => {
+        res.status(500).send(e);
+      });
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+route.delete("/:razon/:pazon", (req, res) => {
+  try {
+    pool
+      .execute("delete from tetel where razon = ? and pazon = ?", [
+        id.parse(Number(req.params.razon)),
+        id.parse(Number(req.params.pazon)),
+      ])
+      .then((r) => res.status(200).json(r))
+      .catch((e) => {
+        res.status(404).send(e);
+      });
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
 
 export default route;
